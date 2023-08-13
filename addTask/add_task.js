044820f0ad41859208aa35a7d248473cf2d386b2
FrkPernille <ullitho@gmx.de>
2023-08-13 09:14:02 +0200

    $*

--- addTask/add_task.js
+++ addTask/add_task.js
@@ -1,2 +1,13 @@
-let allTaske = [];
+let allTasks = [];
 
+//* function to get all values from all inputfields and to push it in an JSON, and then in an array
+function addTask() {
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const date = document.getElementById('calendar');
+    const subtask = document.getElementById('subtask');
+}
+//* function to check which prio is clicked
+function addPrio(clickedTab) {
+
+}
