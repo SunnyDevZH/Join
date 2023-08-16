269d49c369fb0177d01b7e23ed717ddc3d877b8f
Yannick <yannick.vaterlaus@windowslive.com>
2023-08-16 11:49:30 +0200

    All Links in Login

--- login/login.js
+++ login/login.js
@@ -4,9 +4,10 @@ function login(){
     let user = users.find(u => u.email == email.value && u.password == password.value); // Vergleich von Login und Register 
     console.log(user);
     if(user){
+        window.location.href = 'afterLogin';
         console.log('User gefunden') // Weiter leiten an Mainpage!
     } else{
-
+        alert('Falsches Passwort');
     }
 }
 
@@ -17,4 +18,33 @@ if(msg){
     msgBox.innerHTML = msg;
 } else {
     // display:none;
+}
+
+
+
+// Local Storage für Remember me hacken
+
+function save(){
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let remember = document.getElementById('remember').checked;
+
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);     
+}
+
+function load(){
+    let getemail= localStorage.getItem('email');
+    let getpassword = localStorage.getItem('password');
+    
+    document.getElementById('email').value = getemail;
+    document.getElementById('password').value = getpassword;
+}
+
+window.onload = function() {
+    let remember = localStorage.getItem('remember');
+    if (remember === 'true') {
+        load();
+        document.getElementById('remember').checked = true;
+    }
 }
