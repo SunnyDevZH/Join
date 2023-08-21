01bbabaa65dea47c10aed9deae631116a024e35a
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-08-21 23:16:59 +0200

    summary with data binding

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+let todos = [];
+const monthsName = [
+  null,
+  "Januar",
+  "Februar",
+  "März",
+  "April",
+  "Mai",
+  "Juni",
+  "Juli",
+  "August",
+  "September",
+  "Oktober",
+  "November",
+  "Dezember",
+];
+
 function init() {
   setTimeout(renderLogin, 1300);
+}
+
+async function loadTodos() {
+  let newTodos = await getItem("allTasks");
+  todos = JSON.parse(newTodos);
+  for (let i = 0; i < todos.length; i++) {
+    todos[i].id = i;
+  }
+  console.log(todos);
 }
 
 function renderLogin() {
@@ -34,4 +60,33 @@ function renderLogin() {
 
 function guestLogin() {
   window.location.href = "./summary.html";
+}
+
+async function updateSummaryCounter() {
+  await loadTodos();
+  document.getElementById("task-board-counter").innerHTML = todos.length;
+  let todoProgress = todos.filter((t) => t["step"] == "col-02");
+  document.getElementById("todo-inprogress-counter").innerHTML =
+    todoProgress.length;
+  let todoAwait = todos.filter((t) => t["step"] == "col-03");
+  document.getElementById("todo-await-counter").innerHTML = todoAwait.length;
+  let todoOpen = todos.filter((t) => t["step"] == "col-01");
+  document.getElementById("todo-open-counter").innerHTML = todoOpen.length;
+  let todoDone = todos.filter((t) => t["step"] == "col-04");
+  document.getElementById("todo-done-counter").innerHTML = todoDone.length;
+
+  let todoUrgent = todos.filter((t) => t["prio"][0] == "URGENT");
+  document.getElementById("todo-prio-counter").innerHTML = todoUrgent.length;
+
+  let nextdate = "3000-01-01";
+  console.log(nextdate);
+  todoUrgent.forEach((todo) => {
+    if (todo["date"] < nextdate) {
+      nextdate = todo["date"];
+    }
+  });
+  let monthFull = monthsName[parseInt(nextdate.substring(5, 7))];
+  nextdate =
+    nextdate.substring(8) + ". " + monthFull + " " + nextdate.substring(0, 4);
+  document.getElementById("next-date").innerHTML = nextdate;
 }
