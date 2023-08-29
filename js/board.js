391737aff71200817b62b2853d2773c0be6aa7f6
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-08-29 17:43:25 +0200

    delete old board images

--- js/board.js
+++ js/board.js
@@ -1,10 +1,6 @@
 // for testing
 let todos = [];
 
-// const icon_prio_low = "./icons/priority_low.svg";
-// const icon_prio_med = "./icons/priority_medium.svg";
-// const icon_prio_urg = "./icons/priority_urgent.svg";
-
 let currentDraggedElement;
 
 function init() {
