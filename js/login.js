7da07e351f3c54a7a642afc383387cc471b73941
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-08-29 15:23:15 +0200

    get actual user

--- js/login.js
+++ js/login.js
@@ -7,9 +7,12 @@ async function start() {
 function login() {
   let email = document.getElementById("email"); // Eingabe von Login
   let password = document.getElementById("password"); // Eingabe von Login
-  let user = users.find((u) => u.email == email.value && u.password == password.value
+  let user = users.find(
+    (u) => u.email == email.value && u.password == password.value
   ); // Vergleich von Login und Register
   if (user) {
+    let userId = users.findIndex((u) => u.email == email.value);
+    localStorage.setItem("activeID", userId);
     window.location.href = "./summary.html"; // Weiterleitung zum Summary
     alert("Passwort korrekt");
   } else {
